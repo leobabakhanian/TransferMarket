2ffa2aaca27988a357a0f1e06d8b30b1dbe01725
Leo Babakhanian <you@example.com>
2021-10-03 19:21:02 -0700

    update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000;
+const port = process.env.PORT;
 const scrapers = require('./scrapers');
 const db = require('./db');
 const bodyParser = require('body-parser');
