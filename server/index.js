b6ab7ff0cc091ab77d2bff008d354baa46891d20
Leo Babakhanian <babakhanianleo@gmail.com>
2021-10-21 22:14:40 -0700

    fixed broken xpaths

--- server/index.js
+++ server/index.js
@@ -1,33 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const scrapers = require('./scrapers');
-const db = require('./db');
-const bodyParser = require('body-parser');
+const scrapers = require("./scrapers");
+const db = require("./db");
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-})
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
-app.get('/players', async (req, res) => {
+app.get("/players", async (req, res) => {
+  const players = await db.getAllPlayers();
+  res.send(players);
+});
 
-    const players = await db.getAllPlayers();
-    res.send(players);
-})
+app.post("/players", async (req, res) => {
+  console.log(req.body);
+  const playerData = await scrapers.scrapePlayer(req.body.playerURL);
+  const players = await db.insertPlayer(
+    playerData.name,
+    playerData.imageURL,
+    playerData.price,
+    playerData.shirtNum,
+    req.body.playerURL
+  );
+  res.send(players);
+});
 
-app.post('/players', async (req,res) => {
-    console.log(req.body);
-    const playerData = await scrapers.scrapePlayer(req.body.playerURL);
-    const players = await db.insertPlayer(playerData.name, playerData.imageURL, playerData.price, playerData.shirtNum, req.body.playerURL);
-    res.send(players);
-})
-
-app.listen(3000, () => {
-
-})
+app.listen(3000, () => {});
--- server/scrapers.js
+++ server/scrapers.js
@@ -1,45 +1,41 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-async function scrapePlayer(url){
-        
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
+async function scrapePlayer(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
 
-    const [el] = await page.$x('/html/body/div[3]/div[8]/div/div/div[1]/div/div[1]/h1');
-    const text = await el.getProperty('textContent');
-    const name = await text.jsonValue();
+  const [el] = await page.$x(
+    "/html/body/div[3]/div[7]/div/div/div[1]/div/div[1]/h1"
+  );
+  const text = await el.getProperty("textContent");
+  const name = await text.jsonValue();
 
-    const [el2] = await page.$x('/html/body/div[3]/div[8]/div/div/div[3]/div/img');
-    ///html/body/div[3]/div[8]/div/div/div[4]/div/img <-- used in new arrival transfers
-    const src = await el2.getProperty('src');
-    const imageURL = await src.jsonValue();
+  const [el2] = await page.$x(
+    "/html/body/div[3]/div[7]/div/div/div[3]/div/img"
+  );
+  ///html/body/div[3]/div[8]/div/div/div[4]/div/img <-- used in new arrival transfers
+  const src = await el2.getProperty("src");
+  const imageURL = await src.jsonValue();
 
-    const [el3] = await page.$x('/html/body/div[3]/div[8]/div/div/div[5]/a/text()');
-    const textPrice = await el3.getProperty('textContent');
-    const price = await textPrice.jsonValue();
+  const [el3] = await page.$x(
+    "/html/body/div[3]/div[7]/div/div/div[5]/a/text()"
+  );
+  ///html/body/div[3]/div[7]/div/div/div[5]/a/text()
+  const textPrice = await el3.getProperty("textContent");
+  const price = await textPrice.jsonValue();
 
-    const [el4] = await page.$x('/html/body/div[3]/div[8]/div/div/div[1]/div/div[1]/span');
-    const num = await el4.getProperty('textContent');
-    const shirtNum = await num.jsonValue();
-    
-    const [el5] = await page.$x('/html/body/div[3]/div[8]/div/div/div[4]/div[1]/a/img');
-    const clImage = await el5.getProperty('src');
-    const clubImage = await clImage.jsonValue();
+  const [el4] = await page.$x(
+    "/html/body/div[3]/div[7]/div/div/div[1]/div/div[1]/span"
+  );
+  const num = await el4.getProperty("textContent");
+  const shirtNum = await num.jsonValue();
 
-    const [el6] = await page.$x('/html/body/div[3]/div[8]/div/div/div[4]/div[2]/span[1]/a');
-    const clName = await el5.getProperty('textContent');
-    const clubName = await clName.jsonValue();
-
-    const [el7] = await page.$x('/html/body/div[3]/div[8]/div/div/div[2]/div/div[1]/p[3]/span[2]/img');
-    const cntry = await el7.getProperty('src');
-    const country = await cntry.jsonValue();
-
-    browser.close();
-    console.log({name, imageURL, price, shirtNum})
-    return {name, imageURL, price, shirtNum}
+  browser.close();
+  console.log({ name, imageURL, price, shirtNum });
+  return { name, imageURL, price, shirtNum };
 }
 
 module.exports = {
-    scrapePlayer
-}
+  scrapePlayer,
+};
